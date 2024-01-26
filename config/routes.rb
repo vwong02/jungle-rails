5764ea4171b72941d32a2f8932db93e4d1df5d71
Victoria Wong <victoria.ys.wong@gmail.com>
2024-01-26 03:13:50 -0500

    implement categories page

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: %i[edit update show]
+    resources :categories, except: [:edit, :update, :show]
   end
 
   get 'about', to: 'about#index'
