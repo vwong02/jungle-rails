9bc2eb42a6110d08699218ecc835ee4c721f9732
Victoria Wong <victoria.ys.wong@gmail.com>
2024-01-25 23:37:06 -0500

    implement admin dashboard product and category count with styling and links

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,8 @@
 class Admin::DashboardController < ApplicationController
-
   http_basic_authenticate_with name: ENV['HTTP_AUTH_USERNAME'].to_s, password: ENV['HTTP_AUTH_PASSWORD'].to_s
 
-  def show; end
+  def show
+    @product_count = Product.count
+    @category_count = Category.count
+  end
 end
