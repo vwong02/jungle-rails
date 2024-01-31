564bdbda874a4a78d47e167bf8eaff6a5f20eb61
Victoria Wong <victoria.ys.wong@gmail.com>
2024-01-31 14:18:56 -0500

    implement home page tests

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -4,4 +4,12 @@ describe('home page', () => {
     cy.visit('/')
   })
 
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+
 });
