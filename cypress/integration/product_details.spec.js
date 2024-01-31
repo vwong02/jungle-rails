fe580d6fe82f0d8e0c6fdb04866241818e99834c
Victoria Wong <victoria.ys.wong@gmail.com>
2024-01-31 15:12:52 -0500

    implement add to cart tests. Refactor code for tests to select elements instead of contains text

--- cypress/integration/product_details.spec.js
+++ cypress/integration/product_details.spec.js
@@ -2,8 +2,9 @@ describe('product details page', () => {
 
   it('can visit the selected product detail page', () => {
     cy.visit('/')
+    
     cy.get(".products article").first().click()
-    cy.url().should("include", "/products/4")
+    cy.url().should("include", "/products/2")
   })
 
 });
